f4cb60c2e6e81026e871307378fd3fc545bcf618
Chu <root@sh3ll.me>
2023-04-14 01:16:06 +0800

    246. 中心对称数

---
+++ 246-strobogrammatic-number.py
@@ -0,0 +1,23 @@
+import collections
+
+
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        table = collections.defaultdict(lambda: -1,
+                                        {
+                                            '0': '0',
+                                            '1': '1',
+                                            '8': '8',
+                                            '6': '9',
+                                            '9': '6',
+                                        })
+
+        i, j = 0, len(num) - 1
+        while i <= j:
+            if num[j] != table[num[i]]:
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
