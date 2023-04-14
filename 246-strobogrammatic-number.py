a04547c2fcd3fb897c0fd6d971ddc469a02059be
Chu <root@sh3ll.me>
2023-04-15 01:35:06 +0800

    使用 dict 代替 defaultdict

--- 246-strobogrammatic-number.py
+++ 246-strobogrammatic-number.py
@@ -1,20 +1,16 @@
-import collections
-
-
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        table = collections.defaultdict(lambda: '',
-                                        {
-                                            '0': '0',
-                                            '1': '1',
-                                            '8': '8',
-                                            '6': '9',
-                                            '9': '6',
-                                        })
+        table = {
+            '0': '0',
+            '1': '1',
+            '8': '8',
+            '6': '9',
+            '9': '6',
+        }
 
         i, j = 0, len(num) - 1
         while i <= j:
-            if num[j] != table[num[i]]:
+            if num[j] != table.get(num[i]):
                 return False
 
             i += 1
