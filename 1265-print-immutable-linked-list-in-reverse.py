731683f098bd02abdb8f041d874a831710d308b4
Chu <root@sh3ll.me>
2023-05-30 17:11:59 +0800

    1265. 逆序打印不可变链表

---
+++ 1265-print-immutable-linked-list-in-reverse.py
@@ -0,0 +1,5 @@
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+        if head:
+            self.printLinkedListInReverse(head.getNext())
+            head.printValue()
